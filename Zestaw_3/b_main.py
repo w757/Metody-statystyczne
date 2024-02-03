caf208489cf46825ccbe8a95de9c143afa26b7d8
00323Klu <weronika.rzeszutek27@gmail.com>
2024-02-03 02:38:51 -0500

    Zestaw 3

--- Zestaw_3/b_main.py
+++ Zestaw_3/b_main.py
@@ -1,34 +1,17 @@
-# B
-
-# # naa podstawie a 
-
-# C
-
-# # na podstawie a 
-
-
-
-# D
-
-# # jak zmienia sie liczba zadan w systemie w zaleznosci od wspolczynnikow 
-# # pobawic sie symulacja i sprawdzic co sie dzieje w roznych przypadkach 
-# # 
-
-
 import random
 import math
 import matplotlib.pyplot as plt
+import statistics
 # uwzglednic wolne naplywanie, szybkie i stale 
 
-lambda_A = 1/30
-lambda_S = 1/20
+lambda_A = 1/20 #sredni czas wplywania zadania 
+lambda_S = 1/15 # sredni czas wykonywania zadania
 
-tasks = range(0, 10) 
-
-
+tasks = range(0, 100000) 
 
 czas_oczekiwania_tab1 = []
 czas_oczekiwania_tab2 = []
+czas_spedzony_w_systemie = [] 
 
 ilosc = []
 
@@ -37,9 +20,9 @@ czas_oczekiwania = 0
 
 tmp1 =0 
 tmp2 =0
-
+k = 0
 for i in tasks:
-
+    k = k +1
     if (czas_oczekiwania > 0):
         sum_kolejka = sum_kolejka + 1
         ilosc.append(sum_kolejka)
@@ -57,9 +40,7 @@ for i in tasks:
     t_iS = (-1)*math.log(random.uniform(0, 1))/lambda_S
 
 
-    # if (i==0):
-    #     t_i = 0
-    
+    #### tu jest  bload w liczeniu czasu
     czas_oczekiwania = czas_oczekiwania + t_iS - t_i 
 
 
@@ -71,7 +52,16 @@ for i in tasks:
 
     czas_oczekiwania_tab2.append(tmp2)
 
+    czas_spedzony_w_systemie.append(tmp1+tmp2) #czas oczekiwania + czas wykonywania 
 
+
+    ########
+    
+sredni_czas_oczekiwania = statistics.mean(czas_spedzony_w_systemie)
+print ("Ilosc klientw", k)
+print ("sredni_czas_oczekiwania", sredni_czas_oczekiwania)
+print ("lambda_A", lambda_A)
+print (sredni_czas_oczekiwania*lambda_S)
 
 plt.figure(figsize=(15, 5))
 
---
+++ Zestaw_3/d_main.py
@@ -0,0 +1,3 @@
+# # jak zmienia sie liczba zadan w systemie w zaleznosci od wspolczynnikow 
+# # pobawic sie symulacja i sprawdzic co sie dzieje w roznych przypadkach 
+# # 
